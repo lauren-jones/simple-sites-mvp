f58690888157914c88ec6485b5e3377c894596a8
lauren-jones <105777927+lauren-jones@users.noreply.github.com>
2022-11-18 15:34:40 +0000

    Added banner component props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import {useState} from 'react';
 import './App.css';
 import Banner from './Banner';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading</h1>
+        <button onClick={() => setLoading(false)}>Hide loading</button>
+      </div>
+    )
+  } else {
   return (
-    <div className="App">
-      <Banner />
-    </div>
-  );
+      <div className="App">
+        <Banner 
+          title="Images that tell your love story"
+          tagline="Because you should feel like this on your wedding day"
+          image="https://static.showit.co/1600/wpNVgUDES8-bZD1NJp9jRQ/51489/lauren-fair-photography-best-of-2019-luxury-film-destination-wedding-photographer_0313.jpg"
+        />
+      <button onClick={() => setLoading(true)}>Show loading</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Banner.tsx
+++ src/Banner.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
 import './Banner.css';
 
-export default function Banner() {
+export interface BannerProps {
+    image: string,
+    title: string,
+    tagline: string
+}
+
+export default function Banner({ image, title, tagline }: BannerProps) {
   return (
     <div className="banner">
-        <img src="https://static.showit.co/1600/wpNVgUDES8-bZD1NJp9jRQ/51489/lauren-fair-photography-best-of-2019-luxury-film-destination-wedding-photographer_0313.jpg" alt="" className="banner__image"/>
-        <div className="banner__textBox">
-            <h2 className="banner__title">Images that tell your love story</h2>
-            <div className="banner__underline" />
-            <h3 className="banner__tagline">Because you should feel like this on your wedding day</h3>
-        </div>
+      <img src={image} alt="" className="banner__image"/>
+      <div className="banner__textBox">
+        <h2 className="banner__title">{title}</h2>
+        <div className="banner__underline" />
+        <h3 className="banner__tagline">{tagline}</h3>
+      </div>
     </div>
   )
 }
